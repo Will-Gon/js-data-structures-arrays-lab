44d2437e91ad88963492cd10088997cec0045975
Will-Gon <will.colocho@gmail.com>
2020-11-17 17:26:32 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Write your solution here!
+let drivers = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendDriver(name){
+    drivers.push(name)
+}
+
+function destructivelyPrependDriver(name){
+    drivers.unshift(name)
+}
+
+function destructivelyRemoveLastDriver(name){
+    drivers.pop()
+}
+
+function destructivelyRemoveFirstDriver(name){
+    drivers.shift()
+}
+
+function appendDriver(name){
+    let newDriver = [...drivers, name]
+    return newDriver
+}
+
+function prependDriver(name){
+    let addDriver = [name, ...drivers]
+    return addDriver
+}
+
+function removeLastDriver(){
+    let lastDriver = drivers.slice(0, drivers.length - 1)
+    return lastDriver
+}
+
+function removeFirstDriver(){
+    let firstDriver  = drivers.slice(1)
+    return firstDriver
+}
